aa72e89d35473c0ad28c0bfa42c88273bb04adff
annushka34 <annsamoluk82@gmail.com>
2019-10-27 19:40:49 +0300

    shapes

---
+++ Lesson_6_Shapes/02_ViewBox/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_ViewBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Width = 100,
+                Height = 70,
+                Fill = Brushes.Red,
+                Stroke = Brushes.Black,
+                StrokeThickness = 5
+            };
+            Random r = new Random();
+            Canvas.SetTop(ellipse, r.Next(10, 200));
+            canvas.Children.Add(ellipse);
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ellipse_1.Fill = Brushes.BlanchedAlmond;
+        }
+
+        private void Ellipse_1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ellipse_1.Fill = Brushes.Violet;
+        }
+    }
+}
---
+++ Lesson_6_Shapes/Lesson_6_Shapes/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace Lesson_6_Shapes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User user;
+        public MainWindow()
+        {
+            InitializeComponent();
+            user = new User
+            {
+                Name = "Vasiliy",
+                Email = "vas@gmail.com",
+                Age = 25
+            };
+            userStack.DataContext = user;
+        }
+    }
+
+    public class User : IDataErrorInfo
+    {
+        public string this[string columnName]
+        {
+            get
+            {
+            string err = "";
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if(Name.Any(x => !Char.IsLetter(x)))
+                                myError =  "No correct name";
+                            break;
+                        }
+                }
+                return err;
+            }
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string myError { get; set; }
+
+        public string Error
+        {
+            get { return null; }
+        }
+    }
+}
