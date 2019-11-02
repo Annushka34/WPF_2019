0d8c5378c7a5f073071f6499eb73141f0c4c0d32
annushka34 <annsamoluk82@gmail.com>
2019-11-02 23:12:23 +0200

    user control

---
+++ 07_Animations_UserControls/03_UserControls/MyButton.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyButton.xaml
+    /// </summary>
+    public partial class MyButton : UserControl
+    {
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty MyTextProperty = DependencyProperty.Register("MyText", typeof(string), typeof(MyButton), new PropertyMetadata("PRESS ME"));
+
+
+        public Brush MyColor
+        {
+            get { return (Brush)GetValue(MyColorProperty); }
+            set { SetValue(MyColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty MyColorProperty = DependencyProperty.Register("MyColor", typeof(Brush), typeof(MyButton), new PropertyMetadata(Brushes.Red));
+
+        public MyButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 09_Grathics/Test/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           var gr = e.Graphics;///---3
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();//---1
+
+
+
+            Pen p = new Pen(Color.AliceBlue);
+            p.Width = 5;
+            Rectangle r = this.DisplayRectangle;
+            r.Inflate(-20, -20);
+            g.DrawEllipse(p, r);
+            p.Dispose();
+            g.Dispose();
+
+
+
+            Graphics btnG = button1.CreateGraphics();//===2
+            Rectangle r1 = button1.DisplayRectangle;
+            r1.Inflate(-1, -1);
+            Brush b = new SolidBrush(Color.BlueViolet);
+            btnG.FillEllipse(b, r1);
+
+
+            btnG.DrawString("Press me", new Font(FontFamily.GenericMonospace, 12f, FontStyle.Italic), new SolidBrush(Color.Blue), 10, 10);
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Graphics btnG = button1.CreateGraphics();//===2
+            btnG.DrawString("Press me", new Font(FontFamily.GenericMonospace, 12f, FontStyle.Italic), new SolidBrush(Color.Blue), 10, 10);
+        }
+    }
+}
